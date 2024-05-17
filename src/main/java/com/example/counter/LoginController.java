1de156e5e6eea1e8600c58c0b4e54128af80228a
NARRA\emman <ermanfaminiano020@gmail.com>
2024-05-18 01:06:36 +0800

    Initial commit

---
+++ src/main/java/com/example/counter/LoginController.java
@@ -0,0 +1,24 @@
+package com.example.counter;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class LoginController {
+
+    @FXML
+    private Label counterLabel;
+
+    private int counter = 0;
+
+    @FXML
+    protected void onIncrementButtonClick() {
+        counter++;
+        counterLabel.setText(String.valueOf(counter));
+    }
+
+    @FXML
+    protected void onDecrementButtonClick() {
+        counter--;
+        counterLabel.setText(String.valueOf(counter));
+    }
+}
